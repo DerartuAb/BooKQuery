b64223875832247f429f3dfdfa7aab72ad5d55ad
DerartuAb <dabdisa@miu.edu>
2022-11-22 15:42:36 -0600

    Initial commit

---
+++ src/main/java/com/example/bookquery/BooKQueryApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooKQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooKQueryApplication.class, args);
+    }
+
+}
